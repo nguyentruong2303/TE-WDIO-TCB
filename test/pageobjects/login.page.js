101ab5ed6b8cee4c8af682b3651a1dfae14250e1
truong <truong922946@gmail.com>
2023-11-03 09:01:25 +0700

    first upload

---
+++ test/pageobjects/login.page.js
@@ -0,0 +1,48 @@
+import { $ } from '@wdio/globals'
+import Page from './page.js';
+
+const USERNAME_TXB = 'input[name="username"]';
+const PASSWORD_TXB = 'input[name="password"]';
+const LOGIN_BTN = 'input.login-button';
+
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class LoginPage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+     inputUsername (username) {
+         $(USERNAME_TXB).setValue(username);
+         return this;
+    }
+
+     inputPassword (password) {
+         $(PASSWORD_TXB).setValue(password);
+         return this;
+    }
+
+     clickOnLoginBtn () {
+         $(LOGIN_BTN).click();
+    }
+
+    /**
+     * a method to encapsule automation code to interact with the page
+     * e.g. to login using username and password
+     */
+    async login (username, password) {
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    /**
+     * overwrite specific options to adapt it to page object
+     */
+    open () {
+        return super.open('login');
+    }
+}
+
+export default new LoginPage();
